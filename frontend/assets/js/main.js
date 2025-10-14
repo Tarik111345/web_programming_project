0a4bdaa98f0201666083a417218a86eb48ec7958
Tarik111345 <tarik.hadrovic@stu.ibu.edu.ba>
2025-10-14 02:00:40 +0200

    Finished products_list page and spapp.css added and css better managed in the project for home and products pages.

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -3,5 +3,14 @@ $(document).ready(function() {
         defaultView: "home",
         templateDir: "./views/"
     });
+
+    app.route({ view: "home", load: "home.html" });
+    app.route({ view: "products_list", load: "products_list.html" });
+    app.route({ view: "cart", load: "cart.html" });
+    app.route({ view: "checkout", load: "checkout.html" });
+    app.route({ view: "dashboard_user", load: "dashboard_user.html" });
+    app.route({ view: "login", load: "login.html" });
+    app.route({ view: "register", load: "register.html" });
+
     app.run();
 });
